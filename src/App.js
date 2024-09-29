91da28fa3ddee892e380b81743c2f3223dc266f9
TrangDinh524 <dinhtrang.5204@gmail.com>
2024-09-29 23:56:19 +0900

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './NavBar/navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />*/}
+          <Route path='/about' element={<About />} /> 
+        </Routes>
+      </Router>
+    </>
   );
 }
 
